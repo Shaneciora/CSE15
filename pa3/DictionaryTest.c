b69239c7d1453178f50a7576d753ae51b1b9e9f6
Shane Ciora <shaneciora@mac.com>
2019-11-13 14:01:34 -0800

    adding all files

--- pa3/DictionaryTest.c
+++ pa3/DictionaryTest.c
@@ -1,65 +1,52 @@
-//-----------------------------------------------------------------------------
-// DictionaryTest.c
-// Another test client for the Dictionary ADT, made by Shane Ciora
-//-----------------------------------------------------------------------------
+//
+//  DictionaryTest.c
+//  Testing Dictionary ADT
+//
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include"Dictionary.h"
 
-
 int main(){
-   Dictionary A = newDictionary();
-   char* str;
-   char* k;
-   char* v;
-   char* word1[] = {"one","two","three","four","five","six","seven"};
-   char* word2[] = {"foo","bar","blah","galumph","happy","sad","blue"};
-   int i;
+    
+    //--Constructor/Destructor Testing--
+    Dictionary A = newDictionary();
+    //deleteall(A);
+    //freeDictionary(A);
+    
+    //--ADT Operations Testing--
+    
+    //add elements
+    insert(A, "first_key", "first_value");
+    insert(A, "second_key", "second_value");
+    //insert(A, "second_key", "third_value");  //<--should break the program
+    
+//    printf("Size: %d\n", size(A));
 
-   for(i=0; i<7; i++){
-      insert(A, word1[i], word2[i]);
-   }
+//    printf("%s\n",lookup(A, "first_key"));
 
-   // print out the Dictionary
-   str = DictionaryToString(A);
-   printf("%s\n", str);
-   free(str);
+//    delete(A, "first_key");
+//    printf("Size: %d\n", size(A));
+//    printf("%s\n",lookup(A, "first_key"));
 
-   for(i=0; i<7; i++){
-      k = word1[i];
-      v = lookup(A, k);
-      printf("key=\"%s\" %s\"%s\"\n", k, (v==NULL?"not found ":"value="), v);
-   }
-   printf("\n");
+    insert(A, "test1", "test1");
+    insert(A, "test2", "test2");
+    insert(A, "test3", "test3");
+//    printf("%s\n", DictionaryToString(A));
+//    printf("Size: %d\n", size(A));
 
-   // insert(A, "five", "glow"); // duplicate key error
-
-   delete(A, "one");
-   delete(A, "three");
-   delete(A, "seven");
-
-   // print out the Dictionary
-   str = DictionaryToString(A);
-   printf("%s\n", str);
-   free(str);
-
-   for(i=0; i<7; i++){
-      k = word1[i];
-      v = lookup(A, k);
-      printf("key=\"%s\" %s\"%s\"\n", k, (v==NULL?"not found ":"value="), v);
-   }
-   printf("\n");
-
-   // delete(A, "one");  // key not found error
-
-   // check size before and after makeEmpty()
-   printf("%d\n", size(A));
-   makeEmpty(A);
-   printf("%d\n", size(A));
-
-   // free what needs freeing
-   freeDictionary(&A);
-
-   return(EXIT_SUCCESS);
+    delete(A, "test2");
+    printf("%s\n", DictionaryToString(A));
+    printf("Size: %d\n", size(A));
+//
+//    makeEmpty(A);
+//    printf("%s\n", DictionaryToString(A));
+//    printf("Size: %d\n", size(A));
+    
+    freeDictionary(&A);
+    //printf("%s\n", DictionaryToString(A));
+    //printf("Size: %d\n", size(A));
+    
+    return 0;
 }
