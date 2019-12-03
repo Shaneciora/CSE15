76aa671842e59543e6f6b879ec898a3b63578903
Shane Ciora <shaneciora@mac.com>
2019-12-02 19:37:39 -0800

    changed

--- pa4/IntegerQueueClient.c
+++ pa4/IntegerQueueClient.c
@@ -4,98 +4,87 @@
 //-----------------------------------------------------------------------------
 #include<stdio.h>
 #include<stdlib.h>
-#include"IntegerQueue.h"
+#include"IntegerQueue.h" 
 
 int main(){
 
-    int i, j;
-    char* strA;
-    char* strB;
-    char* strC;
+   int i, j;
+   char* strA;
+   char* strB;
 
-    IntegerQueue A = newIntegerQueue();
-    IntegerQueue B = newIntegerQueue();
-    IntegerQueue C = newIntegerQueue();
+   IntegerQueue A = newIntegerQueue();
+   IntegerQueue B = newIntegerQueue();
 
-    printf("%s\n", isEmpty(A)?"true":"false");
-    printf("%s\n", isEmpty(B)?"true":"false");
-    printf("%s\n", isEmpty(C)?"true":"false");
+   printf("%s\n", isEmpty(A)?"true":"false");
+   printf("%s\n", isEmpty(B)?"true":"false");
 
-    enqueue(C, 5);
-    enqueue(C, 3);
-    enqueue(A, 9);
-    enqueue(A, 7);
-    enqueue(A, 8);
+   enqueue(A, 5);
+   enqueue(A, 3);
+   enqueue(A, 9);
+   enqueue(A, 7);
+   enqueue(A, 8);
 
-    printf("%s\n", isEmpty(C)?"true":"false");
-    printf("%s\n", isEmpty(A)?"true":"false");
-    
-    strC = IntegerQueueToString(C);
-    printf("%s\n", strC);
-    free(strC);
-    
-    strA = IntegerQueueToString(A);
-    printf("%s\n", strA);
-    free(strA);
+   printf("%s\n", isEmpty(A)?"true":"false");
 
+   strA = IntegerQueueToString(A);
+   printf("%s\n", strA);
+   free(strA);
 
-    printf("%d\n", peek(A));
-    
-    dequeue(A);
-    dequeue(A);
-    dequeue(A);
-    
-    printf("%d\n", peek(A));
-    
-    strA = IntegerQueueToString(A);
-    printf("PRINT A: %s\n", strA);
-    free(strA);
+   printf("%d\n", peek(A));
+   dequeue(A);
+   dequeue(A);
+   dequeue(A);
+   printf("%d\n", peek(A));
 
-    enqueue(B, 7);
-    enqueue(B, 8);
+   strA = IntegerQueueToString(A);
+   printf("%s\n", strA);
+   free(strA);
 
-    strB = IntegerQueueToString(B);
-    printf("PRINT B: %s\n", strB);
-    free(strB);
+   enqueue(B, 7);
+   enqueue(B, 8);
 
-    printf("%s\n", equals(A, B)?"true":"false");
+   strB = IntegerQueueToString(B);
+   printf("%s\n", strB);
+   free(strB);
 
-    enqueue(A, 12);
-    enqueue(B, 13);
+   printf("%s\n", equals(A, B)?"true":"false");
 
-    strA = IntegerQueueToString(A);
-    printf("%s\n", strA);
-    free(strA);
+   enqueue(A, 12);
+   enqueue(B, 13);
 
-    strB = IntegerQueueToString(B);
-    printf("%s\n", strB);
-    free(strB);
+   strA = IntegerQueueToString(A);
+   printf("%s\n", strA);
+   free(strA);
 
-    printf("%s\n", equals(A, B)?"true":"false");
+   strB = IntegerQueueToString(B);
+   printf("%s\n", strB);
+   free(strB);
 
-    dequeueAll(A);
-    dequeueAll(B);
-    printf("%s\n", isEmpty(A)?"true":"false");
-    printf("%s\n", isEmpty(B)?"true":"false");
+   printf("%s\n", equals(A, B)?"true":"false");
 
-    freeIntegerQueue(&A);
-    freeIntegerQueue(&B);
+   dequeueAll(A);
+   dequeueAll(B);
+   printf("%s\n", isEmpty(A)?"true":"false");
+   printf("%s\n", isEmpty(B)?"true":"false");
 
-    A = newIntegerQueue();
-    B = newIntegerQueue();
+   freeIntegerQueue(&A);
+   freeIntegerQueue(&B);
 
-    for(i=0; i<10; i++){
+   A = newIntegerQueue();
+   B = newIntegerQueue();
+
+   for(i=0; i<10; i++){
       enqueue(A, i);
-    }
+   }
 
-    strA = IntegerQueueToString(A);
-    strB = IntegerQueueToString(B);
-    printf("A: %s\n", strA);
-    printf("B: %s\n", strB);
-    free(strA);
-    free(strB);
+   strA = IntegerQueueToString(A);
+   strB = IntegerQueueToString(B);
+   printf("A: %s\n", strA);
+   printf("B: %s\n", strB);
+   free(strA);
+   free(strB);
 
-    while( !isEmpty(A) ){
+   while( !isEmpty(A) ){
       j = dequeue(A);
       enqueue(B, j);
       if( j%3==0 ){
@@ -108,12 +97,12 @@ int main(){
       printf("B: %s\n", strB);
       free(strA);
       free(strB);
-    }
+   }
 
-    freeIntegerQueue(&A);
-    freeIntegerQueue(&B);
+   freeIntegerQueue(&A);
+   freeIntegerQueue(&B);
 
-    return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }
 
 /* Output of this program: ****************************************************
